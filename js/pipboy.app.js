5f8f47c91caa5093389b7b025cec9915359e75d0
wldbs31 <jiyunchoi31@gmail.com>
2024-07-04 13:54:08 -0400

    Inv tab was made and the tab pane is added

---
+++ js/pipboy.app.js
@@ -0,0 +1,29 @@
+$(document).ready(function ()){
+    var weapons = [{
+        "name": "44_pistol",
+        "damage": 48,
+        "fire_rate": 6,
+        "Range": 119,
+        "accuracy": 66,
+        "weight": 4.2,
+        "value": 99
+    },
+    {
+        "name": "10mm pistol",
+        "damage": 18,
+        "fire_rate": 46,
+        "Range": 119,
+        "accuracy": 61,
+        "weight": 4.2,
+        "value": 53
+    },
+    {
+        "name": "Assault rifle",
+        "damage": 30,
+        "fire_rate": 40,
+        "Range": 119,
+        "accuracy": 72,
+        "weight": 13.1,
+        "value": 144
+    }]
+}
