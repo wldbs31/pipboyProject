5faaee4219326a05e8f04a49e3e209e10fdf36ad
wldbs31 <jiyunchoi31@gmail.com>
2024-07-06 17:46:58 -0400

    first page updated

--- js/pipboy.app.js
+++ js/pipboy.app.js
@@ -1,29 +1,56 @@
-$(document).ready(function ()){
+$(document).ready(function () {
     var weapons = [{
-        "name": "44_pistol",
+        "name": "44-pistol",
         "damage": 48,
         "fire_rate": 6,
-        "Range": 119,
+        "range": 119,
         "accuracy": 66,
         "weight": 4.2,
         "value": 99
     },
     {
-        "name": "10mm pistol",
+        "name": "10mm-pistol",
         "damage": 18,
         "fire_rate": 46,
-        "Range": 119,
+        "range": 119,
         "accuracy": 61,
         "weight": 4.2,
         "value": 53
     },
     {
-        "name": "Assault rifle",
+        "name": "assault-rifle",
         "damage": 30,
         "fire_rate": 40,
-        "Range": 119,
+        "range": 119,
         "accuracy": 72,
         "weight": 13.1,
         "value": 144
-    }]
-}
+    }
+    ];
+
+    $('.item-list a').on('click', function (e) {
+        $('.item-list a').removeClass('active');
+        $(e.currentTarget).addClass('active');
+    });
+
+
+    $('.item-list a').on('mouseenter', function (e) {
+        var current_item = $(e.currentTarget).attr('class');
+        console.log(current_item);
+
+        for (item in weapons) {
+            if (weapons[item].name == current_item) {
+                console.log(weapons[item]);
+
+                var container = $('.item-stats');
+                container.find('.damage').html(weapons[item].damage);
+                container.find('.fire_rate').html(weapons[item].fire_rate);
+                container.find('.accuracy').html(weapons[item].accuracy);
+                container.find('.range').html(weapons[item].Range);
+                container.find('.weight').html(weapons[item].weight);
+                container.find('.value').html(weapons[item].value);
+            }
+        }
+    });
+
+});
